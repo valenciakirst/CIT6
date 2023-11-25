ed2572174611fe94d17e0e54724e69ac6aa45f5f
valenciakirst <candykaye87@gmail.com>
2023-11-25 20:58:30 +0800

    New Update uwu

--- app/src/main/java/com/example/mrhydro/HomeFragment.java
+++ app/src/main/java/com/example/mrhydro/HomeFragment.java
@@ -1,20 +1,46 @@
 package com.example.mrhydro;
 
 import android.os.Bundle;
-
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-
+import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        CardView tempcard = view.findViewById(R.id.TempCard);
+        CardView humiditycard = view.findViewById(R.id.HumidityCard);
+        CardView mistercard = view.findViewById(R.id.MisterCard);
+
+        tempcard.setOnClickListener(this);
+        humiditycard.setOnClickListener(this);
+        mistercard.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.TempCard) {
+            openFragment(new TemperatureFragment());
+        } else if (v.getId() == R.id.HumidityCard) {
+            openFragment(new HumidityFragment());
+        } else if (v.getId() == R.id.MisterCard) {
+            openFragment(new MisterFragment());
+        }
+    }
+
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
