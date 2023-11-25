f7e277130fb84e200b501a5b9c04706b12b69a26
HeneralLoona <salgadoriva@gmail.com>
2023-11-25 20:46:35 +0800

    changes

--- app/src/main/java/com/example/mrhydro/HomeFragment.java
+++ app/src/main/java/com/example/mrhydro/HomeFragment.java
@@ -1,20 +1,31 @@
 package com.example.mrhydro;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+    private CardView temperature;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        //temperature = (CardView) view.findViewById(R.id.temperature);
+
+        return view;
     }
-}
+       //temperature.setOnClickListener(new View.OnClickListener() {
+            public void onLinearLayoutClick(View view) {
+                Intent i = new Intent(getActivity(), Temperature.class);
+                startActivity(i);
+            }
+        }
+
+
+
