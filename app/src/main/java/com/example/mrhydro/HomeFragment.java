ead3e2cc870e215d34d5c055828b45793e5f7188
valenciakirst <candykaye87@gmail.com>
2023-12-15 11:43:09 +0800

    HomeFragment and fragment_home changes

--- app/src/main/java/com/example/mrhydro/HomeFragment.java
+++ app/src/main/java/com/example/mrhydro/HomeFragment.java
@@ -1,26 +1,39 @@
 package com.example.mrhydro;
 
-import android.content.Intent; // Add this import statement
+import android.content.Intent;
 import android.os.Bundle;
-import androidx.cardview.widget.CardView;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.example.mrhydro.databinding.ActivityMainBinding;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.mrhydro.databinding.FragmentHomeBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
-    // ...
+    private static final int UPDATE_INTERVAL = 5000;
+
+    FragmentHomeBinding binding;
+    DatabaseReference reference;
+    Handler handler = new Handler(Looper.getMainLooper());
+    private String humidityValue; // Declare humidityValue as a class-level variable
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
         CardView tempcard = view.findViewById(R.id.TempCard);
         CardView humiditycard = view.findViewById(R.id.HumidityCard);
@@ -31,6 +44,10 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         humiditycard.setOnClickListener(this);
         mistercard.setOnClickListener(this);
         notificationbt.setOnClickListener(this);
+
+        readHumidityData();
+
+        handler.postDelayed(updateRunnable, UPDATE_INTERVAL);
 
         return view;
     }
@@ -55,5 +72,41 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         transaction.addToBackStack(null);
         transaction.commit();
     }
-}
 
+    private void readHumidityData() {
+        reference = FirebaseDatabase.getInstance().getReference("DHT");
+        reference.child("Humidity").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getValue() instanceof Double) {
+                    humidityValue = String.valueOf(dataSnapshot.getValue());
+                    Log.d("HumidityFragment", "Humidity value from Firebase: " + humidityValue);
+
+                    if (humidityValue != null && !humidityValue.isEmpty()) {
+                        binding.humidityValue.setText(humidityValue);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("HumidityFragment", "Failed to read humidity data", databaseError.toException());
+            }
+        });
+    }
+
+    private Runnable updateRunnable = new Runnable() {
+        @Override
+        public void run() {
+            readHumidityData();
+            handler.postDelayed(this, UPDATE_INTERVAL);
+        }
+    };
+
+    @Override
+    public void onDestroyView() {
+        // Remove the callbacks to prevent memory leaks
+        handler.removeCallbacks(updateRunnable);
+        super.onDestroyView();
+    }
+}
