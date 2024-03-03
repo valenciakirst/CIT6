99ac86b13a0b25418bf9919226a1ed02821abfe2
HeneralLoona <salgadoriva@gmail.com>
2024-03-03 20:32:50 +0800

    ignore this commit

--- app/src/main/java/com/example/mrhydro/HumidityFragment.java
+++ app/src/main/java/com/example/mrhydro/HumidityFragment.java
@@ -12,7 +12,6 @@ import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
